3b9582240b78bf84ce09a675ddf8bf4fc3279259
Seth Villalon <sethvillalon95@gmail.com>
2020-12-22 17:21:34 +0800

    Ask for number of terms

--- Calculus-App/src/CalculusMain.java
+++ Calculus-App/src/CalculusMain.java
@@ -1,8 +1,25 @@
+import java.util.Scanner; //
 
-public class CalculusMain {
+public class CalculusMain { 	
+	public CalculusMain() {
+		Scanner s = new Scanner(System.in);
+
+		int numberOfTerms = 0;
+		System.out.println("Derivatives");
+		System.out.print("How many terms = ");
+	    numberOfTerms = s.nextInt();
+
+		System.out.println("Prining valu of numberOfTerms "+numberOfTerms);
+		
+		// make a for loop that asks contiuously for the values 
+		
+	}
+		
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Running...");
+		new CalculusMain();
 
 	}
 
