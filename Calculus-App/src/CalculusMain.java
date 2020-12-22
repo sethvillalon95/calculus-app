8cfd6a428b1be98c9230e5221fb466c52e880f3e
Seth Villalon <sethvillalon95@gmail.com>
2020-12-22 22:16:54 +0800

    Add term class

--- Calculus-App/src/CalculusMain.java
+++ Calculus-App/src/CalculusMain.java
@@ -7,7 +7,23 @@ public class CalculusMain {
 		int numberOfTerms = 0;
 		System.out.println("Derivatives");
 		System.out.print("How many terms = ");
+		// make a class foreach term
 	    numberOfTerms = s.nextInt();
+	    
+	    //int terms[numberOfTerms];
+	    
+	    term[] terms = new term[numberOfTerms];
+	    for(int i = 0; i<numberOfTerms; i++) {
+	    	System.out.println("Term " + i +" "); 
+	    	terms[i] = new term();
+	    	term tempTerm = terms[i];
+	    	tempTerm.coefficient = s.nextInt();
+	    	tempTerm.exponent = s.nextInt();
+	    	
+	    }
+	    
+	    // ask for the user inputs 
+	    
 
 		System.out.println("Prining valu of numberOfTerms "+numberOfTerms);
 		
---
+++ Calculus-App/src/term.java
@@ -0,0 +1,20 @@
+
+public class term {
+	int coefficient = 1;
+	int exponent = 1;
+	
+	public term() {
+		
+	}
+	
+	public term(int c, int e) {
+		coefficient =c;
+		exponent = e; 
+	}
+	
+	public term(int c) {
+		coefficient =c;
+	}
+	
+	
+}
