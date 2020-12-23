7b0ff8e47072be088dd103ccc6d5e27d113c8d09
Seth Villalon <sethvillalon95@gmail.com>
2020-12-23 20:57:51 +0800

    Start console app optimization

--- Calculus-App/src/term.java
+++ Calculus-App/src/term.java
@@ -3,6 +3,8 @@ public class term {
 	int coefficient = 1;
 	int exponent = 1;
 	
+	
+	// do the calculations here for better encapsulation 
 	public term() {
 		
 	}
@@ -15,6 +17,33 @@ public class term {
 	public term(int c) {
 		coefficient =c;
 	}
+	
+	public void compute() {
+		int resultCoefficient = coefficient*exponent;
+		int resultExponent = exponent-1;
+		
+		coefficient = resultCoefficient;
+		exponent = resultExponent;
+		
+	}
+	
+	public String addResult() {
+		String finalResult =" ";
+		if(coefficient != 1 && coefficient != 0) {
+			// convert int to string 
+			String tempCoef = Integer.toString(coefficient);
+			String tempExp = Integer.toString(exponent);
+			
+			
+
+			
+			// return 
+		}
+		return finalResult;
+		
+	}
+	
+	
 	
 	
 }
